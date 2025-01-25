ffefca054abdcdef67d48e4e3bed4d0d2284c5e9
Jose Ysique <joseysique@gmail.com>
2025-01-25 15:31:35 -0500

    fix scene

---
+++ Assets/Scripts/Controlers/ScreenCutAnimation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCutAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(EndCutAnimation),1f);
+    }
+
+
+    void EndCutAnimation(){
+        SceneGameManager.Instance.GoToGame();
+    }
+}
