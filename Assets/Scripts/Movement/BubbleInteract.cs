45cb54878d074b593e73a922920f05ee59afcd98
Jose Ysique <joseysique@gmail.com>
2025-01-25 03:07:47 -0500

    fix gravity

--- Assets/Scripts/Movement/BubbleInteract.cs
+++ Assets/Scripts/Movement/BubbleInteract.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BubbleInteract : MonoBehaviour
 {
     public float force = 10f;
         private Rigidbody2D rb;
-        public ArrowFollow arrowFollow;
+     
     // Start is called before the first frame update
     void Start()
     {
